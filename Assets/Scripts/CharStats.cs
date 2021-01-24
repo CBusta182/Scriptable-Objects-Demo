5f2f4815abde165be88b16df46d4e1c685c894ba
Chris Bustamante <68354135+CBusta182@users.noreply.github.com>
2021-01-24 16:46:24 -0500

    Add files via upload

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Character", menuName
+= "Player Units")]
+public class CharStats : ScriptableObject
+{
+    public string charName;
+    public int attack;
+    public int defense;
+    public int speed;
+    public int maxHealth;
+    public Material newColor;
+    public void PrintMessage()
+    {
+        Debug.Log("The " + charName + " character has been loaded.");
+    }
+    public void RandomizeStats()
+    {
+        attack = Random.Range(1, 20);
+        defense = Random.Range(1, 20);
+        speed = Random.Range(1, 20);
+        maxHealth = Random.Range(1, 20);
+    }
+}
---
+++ Assets/Scripts/LoadStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadStats : MonoBehaviour
+{
+    public CharStats charStat;
+    public Text nameText;
+    public Text attackText;
+    public Text defenseText;
+    public Text speedText;
+    public Text maxHealthText;
+    public MeshRenderer currentColor;
+    void Start()
+    {
+        DisplayStats();
+        charStat.PrintMessage();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charStat.RandomizeStats();
+            DisplayStats();
+        }
+    }
+    void DisplayStats()
+    {
+        nameText.text = "Name: " + charStat.charName;
+        attackText.text = "Attack: " + charStat.attack;
+        defenseText.text = "Defense: " + charStat.defense;
+        speedText.text = "Speed: " + charStat.speed;
+        maxHealthText.text = "Max Health: " + charStat.maxHealth;
+        currentColor.material = charStat.newColor;
+    }
+}
